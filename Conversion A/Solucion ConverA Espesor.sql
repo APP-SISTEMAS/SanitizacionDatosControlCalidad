1b38968db79f2c8dfef61a752e50d168ef9d2875
Ariel Alvarado <arielalvarado15@protonmail.com>
2024-04-09 16:19:40 -0600

    Solucion ConverA-B Espesor (Finalizado[Contiene Updates])

---
+++ Conversion A/Solucion ConverA Espesor.sql
@@ -0,0 +1,34 @@
+use APP_SISTEMAS
+
+-------------------------------------------------------------------
+/*NO TOCAR,en buen estado*/
+select ESPESOR,ESPESOR as valor 
+from SIQM_ENC_AUDI_CONVE_A
+where TRY_CAST(espesor as float) >10 
+group by ESPESOR 
+union all
+
+-------------------------------------------------------------------
+/*caso especial, NULOS*/
+select ESPESOR, 0
+from SIQM_ENC_AUDI_CONVE_A
+where espesor is null 
+group by ESPESOR
+union all
+
+-------------------------------------------------------------------
+/*caso #1 solo numeros*/
+select ESPESOR,
+round(cast(ESPESOR as float)*25.4,0)
+from SIQM_ENC_AUDI_CONVE_A
+where TRY_CAST(espesor as float) <10 
+group by ESPESOR 
+
+update SIQM_ENC_AUDI_CONVE_A
+set ESPESOR=round(cast(ESPESOR as float)*25.4,0)
+where TRY_CAST(espesor as float) <10 
+
+go
+-- Convirtiendo el campo a tipo Numerico
+ALTER TABLE  SIQM_ENC_AUDI_conve_A ALTER COLUMN espesor DECIMAL(15,2)
+GO 
